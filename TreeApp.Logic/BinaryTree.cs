e16d74ded56896127b8d6244290acd8037372b76
jacenielson <jace.nielson@students.snow.edu>
2026-03-13 10:06:54 -0600

    working on phantom nodes

--- TreeApp.Logic/BinaryTree.cs
+++ TreeApp.Logic/BinaryTree.cs
@@ -23,7 +23,7 @@ public class BinaryTree
                 RecursiveInsert(current.Left, value);
             }
         }
-        if (value > current.Valye)
+        if (value > current.Value)
         {
             if(current.Right == null)
             {
@@ -45,7 +45,45 @@ public class BinaryTree
     }
     public string ToMermaid()
     {
-        return @"graph TD
-         5 ---> 2";
+        if(Root == null)
+        {
+            return "graph TD;\n";
+        }
+        else if (Root.Left == null && Root.Right == null)
+        {
+            return $"graph TD;\n{Root.Value}\n";
+        }
+        return $"graph TD;\n{ToMermaid(Root)}\n";
+    }
+    private string ToMermaid(Node? node)
+    {
+        if (node == null || node.Right == null && node.Left == null)
+        {
+            return string.Empty;
+        }
+        string result = string.Empty;
+        if(node.Left != null)
+        {
+            result += $"{node.Value} --> {node.Left.Value} \n";
+            result += ToMermaid(node.Left);
+        }
+        else
+        {
+            result += $"{node.Value} --> _phl{node.Value}[ ] \n";
+            result += "linkStyle 1 stroke:none,stroke-width:0,fill:none \n";
+            result += $"style _phl{node.Value} fill:none,stroke:none,color:none \n";
+        }
+        if(node.Right != null)
+        {
+            result += $"{node.Value} --> {node.Right.Value} \n";
+            result += ToMermaid(node.Right);
+        }
+        else
+        {
+            result += $"{node.Value} --> _phr{node.Value}[ ] \n";
+            result += "linkStyle 1 stroke:none,stroke-width:0,fill:none \n";
+            result += $"style _phr{node.Value} fill:none, stroke:none,color:none \n";
+        }
+        return result;
     }
 }
